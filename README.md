84b744383d1978050df5095f5cba5d38d69173a1
Diego Núñez Silva <diego.nunez.mx@gmail.com>
2020-08-11 09:01:10 +0200

    Add initial version

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # github-webook-receiver
-A nodejs simple application that receives webhooks events from Github
+A nodejs simple application that receives webhooks events from Github.
+
+Inspired from [tutorial](https://www.robinwieruch.de/github-webhook-node-js).
---
+++ github-webook-receiver.js
@@ -0,0 +1,44 @@
+require('dotenv').config({
+    path: __dirname + '/.env'
+});
+
+const http = require('http');
+const crypto = require('crypto');
+const {
+    exec
+} = require('child_process');
+
+const SECRET = process.env['SECRET'];
+
+http
+    .createServer((req, res) => {
+        req.on('data', chunk => {
+            const signature = `sha1=${crypto
+        .createHmac('sha1', SECRET)
+        .update(chunk)
+        .digest('hex')}`;
+
+            console.log(`CHUNK=${chunk}`);
+
+            const isAllowed = req.headers['x-hub-signature'] === signature;
+
+            const body = JSON.parse(chunk);
+
+            const isMaster = body.ref === 'refs/heads/master';
+
+            if (isAllowed && isMaster) {
+
+                const repository = body.repository.name;
+                console.log(`Push detected for repository ${repository}...`);
+
+                try {
+                    exec(`cd /home/pi/wa/${repository} && git pull && systemctl restart ${repository}`);
+                } catch (error) {
+                    console.log(error);
+                }
+            }
+        });
+
+        res.end();
+    })
+    .listen(9530);
